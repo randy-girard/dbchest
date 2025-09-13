76300fb7840422b94ad2051efc43c62f566783b8
Randy Girard <randy@tradepending.com>
2025-09-13 09:32:44 -0400

    chore: make sure there is a node

--- app/services/create_service.rb
+++ app/services/create_service.rb
@@ -1,14 +1,15 @@
 class CreateService
   include Sidekiq::Job
-  
+
   def initialize
   end
 
   def perform(node_id)
     @node = Node.find_by_id(node_id)
+    if @node
+      TerraformCreateService.new.perform(@node.id)
 
-    TerraformCreateService.new.perform(@node.id)
-
-    AnsibleRunService.new.perform(@node.id)
+      AnsibleRunService.new.perform(@node.id)
+    end
   end
 end
